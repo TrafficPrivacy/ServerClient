0aeddc0029b5ef4649f48f7aa6e5521d99a674b9
Zonglin Li <lizonglin6565@163.com>
2017-10-04 00:02:34 -0500

    changed to maven part 2

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
+wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/download.mapsforge.org/maps/north-america/us/illinois.map
