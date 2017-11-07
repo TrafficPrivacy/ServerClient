558e829aa780308ddd8c87dce77103b49f29bf55
Zonglin Li <lizonglin6565@163.com>
2017-11-07 17:41:13 -0600

    added new helpers

---
+++ src/main/java/util/MutableInt.java
@@ -0,0 +1,25 @@
+package util;
+
+public class MutableInt {
+    private int mValue;
+
+    public MutableInt() {
+        this(0);
+    }
+
+    public MutableInt(int initialVal) {
+        mValue = initialVal;
+    }
+
+    public void increment() {
+        mValue++;
+    }
+
+    public int get() {
+        return mValue;
+    }
+
+    public void set(int value) {
+        mValue = value;
+    }
+}
---
+++ src/main/java/util/Segment.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Segment extends Pair<MapPoint, MapPoint> {
+
+    public Segment(MapPoint first, MapPoint second) {
+        super(first, second);
+    }
+
+    public MapPoint getStart() {
+        return mFirst;
+    }
+
+    public MapPoint getEnd() {
+        return mSecond;
+    }
+
+    public String toString() {
+        return mFirst.toString() + "," + mSecond.toString();
+    }
+
+}
