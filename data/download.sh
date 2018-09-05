89a4b9a8a07dd22a0f74d4835303fbe76ade611b
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-09-04 22:24:41 -0500

    Updated data download url

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 wget https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
 wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/download.mapsforge.org/maps/v4/north-america/us/illinois.map
+wget https://download.geofabrik.de/north-america/us/new-york-latest.osm.pbf
+# See the full list of trip data here:
+# https://github.com/toddwschneider/nyc-taxi-data/blob/master/raw_data_urls.txt
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-12.csv
