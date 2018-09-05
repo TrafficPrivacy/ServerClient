f939ccf87cd2084f15160b6de839a02d8ec05c94
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-09-04 19:48:52 -0500

    updated download link

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
-wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/download.mapsforge.org/maps/north-america/us/illinois.map
+wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/download.mapsforge.org/maps/v4/north-america/us/illinois.map
