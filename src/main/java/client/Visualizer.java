cc61b3f22d6a16ee7fcd83daf1c6d0a3a3c9b445
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-11-06 23:53:15 -0600

    Made dot more visible

--- src/main/java/client/Visualizer.java
+++ src/main/java/client/Visualizer.java
@@ -2,12 +2,10 @@ package client;
 
 import com.graphhopper.storage.NodeAccess;
 import java.awt.Dimension;
-import java.awt.event.ContainerEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.prefs.Preferences;
 import javax.swing.JFrame;
@@ -25,6 +23,7 @@ import org.mapsforge.map.awt.util.JavaPreferences;
 import org.mapsforge.map.awt.view.MapView;
 import org.mapsforge.map.datastore.MapDataStore;
 import org.mapsforge.map.datastore.MultiMapDataStore;
+import org.mapsforge.map.layer.Layer;
 import org.mapsforge.map.layer.Layers;
 import org.mapsforge.map.layer.cache.FileSystemTileCache;
 import org.mapsforge.map.layer.cache.InMemoryTileCache;
@@ -32,12 +31,9 @@ import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.cache.TwoLevelTileCache;
 import org.mapsforge.map.layer.debug.TileCoordinatesLayer;
 import org.mapsforge.map.layer.debug.TileGridLayer;
-import org.mapsforge.map.layer.overlay.Circle;
 import org.mapsforge.map.layer.overlay.Polygon;
-import org.mapsforge.map.layer.overlay.Polyline;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.model.IMapViewPosition;
-import org.mapsforge.map.model.MapViewPosition;
 import org.mapsforge.map.model.Model;
 import org.mapsforge.map.model.common.PreferencesFacade;
 import org.mapsforge.map.reader.MapFile;
@@ -45,7 +41,6 @@ import org.mapsforge.map.rendertheme.InternalRenderTheme;
 import util.Convex;
 import util.Logger;
 import util.MapPoint;
-import util.MapPoint.LatLongAdapter;
 import util.Pair;
 
 public final class Visualizer {
@@ -55,8 +50,7 @@ public final class Visualizer {
 
   private final MapView MAP_VIEW;
   private final JFrame FRAME;
-  private final float STROKEWIDTH = 1.0f;
-  private final float BORDERSTROKEWIDTH = 1.0f;
+  private final float BORDERSTROKEWIDTH = 2.0f;
   private final java.awt.Color DOTCOLOR = new java.awt.Color(31, 46, 250, 255);
   private final java.awt.Color FILLCOLOR = new java.awt.Color(255, 0, 4, 63);
   private final java.awt.Color BORDERCOLOR = new java.awt.Color(255, 0, 4, 164);
@@ -114,10 +108,13 @@ public final class Visualizer {
     for (Pair<int[], int[]> pair : regions) {
       int[] points = pair.mFirst;
       int[] borderPoints = pair.mSecond;
+      ArrayList<MapPoint> allPoints = new ArrayList<>();
       for (int point : points) {
         MapPoint mapPoint = MapPoint.convertFromGHPointIndex(point, mNodeAccess);
-        createDot(new LatLongAdapter(mapPoint), STROKEWIDTH);
+        allPoints.add(mapPoint);
       }
+      DotsLayer dotsLayer = new DotsLayer(MapPoint.convertToLatlong(allPoints), 5);
+      MAP_VIEW.getLayerManager().getLayers().add(dotsLayer);
       ArrayList<MapPoint> borders = new ArrayList<>();
       for (int borderPoint : borderPoints) {
         borders.add(MapPoint.convertFromGHPointIndex(borderPoint, mNodeAccess));
@@ -125,10 +122,6 @@ public final class Visualizer {
       List<LatLong> convex = MapPoint.convertToLatlong(Convex.getConvex(borders));
       createPolygon(convex, BORDERSTROKEWIDTH);
     }
-  }
-
-  public void createDot(LatLong coordinates, float strokeWidth) {
-    createCircle(coordinates, DOTCOLOR.getRGB(), 3, true);
   }
 
   public void createPolygon(List<LatLong> coordinates, float strokeWidth) {
@@ -144,32 +137,6 @@ public final class Visualizer {
     MAP_VIEW.getLayerManager().redrawLayers();
   }
 
-  public void createPolyline(List<LatLong> coordinates, int color, float strokeWidth) {
-    org.mapsforge.core.graphics.Paint paintStroke = GRAPHIC_FACTORY.createPaint();
-    paintStroke.setStyle(Style.STROKE);
-    paintStroke.setColor(color);
-    paintStroke.setStrokeWidth(strokeWidth);
-    Polyline pl = new Polyline(paintStroke, GRAPHIC_FACTORY);
-    pl.getLatLongs().addAll(coordinates);
-    MAP_VIEW.getLayerManager().getLayers().add(pl);
-    MAP_VIEW.getLayerManager().redrawLayers();
-  }
-
-  public int createCircle(LatLong latLong, int color, float radius, boolean fill) {
-    org.mapsforge.core.graphics.Paint paintStroke = GRAPHIC_FACTORY.createPaint();
-    paintStroke.setStyle(Style.STROKE);
-    paintStroke.setColor(color);
-    paintStroke.setStrokeWidth(STROKEWIDTH);
-    org.mapsforge.core.graphics.Paint paintFill = GRAPHIC_FACTORY.createPaint();
-    paintFill.setColor(color);
-    Circle circle = new Circle(latLong, radius, fill ? paintFill : null, paintStroke);
-    MAP_VIEW.getLayerManager().getLayers().add(circle);
-    MAP_VIEW.getLayerManager().redrawLayers();
-    return 0;
-  }
-
-  /////// Helper Functions ///////
-
   private BoundingBox addLayers(MapView mapView, List<File> mapFiles) {
     Layers layers = mapView.getLayerManager().getLayers();
 
@@ -179,7 +146,7 @@ public final class Visualizer {
     for (File file : mapFiles) {
       mapDataStore.addMapDataStore(new MapFile(file), false, false);
     }
-    TileRendererLayer tileRendererLayer = createTileRendererLayer(createTileCache(64), mapDataStore,
+    TileRendererLayer tileRendererLayer = createTileRendererLayer(createTileCache(), mapDataStore,
         mapView.getModel().mapViewPosition);
     layers.add(tileRendererLayer);
     BoundingBox boundingBox = mapDataStore.boundingBox();
@@ -193,6 +160,8 @@ public final class Visualizer {
     return boundingBox;
   }
 
+  /////// Helper Functions ///////
+
   private MapView createMapView() {
     MapView mapView = new MapView();
     mapView.getMapScaleBar().setVisible(true);
@@ -203,8 +172,8 @@ public final class Visualizer {
     return mapView;
   }
 
-  private TileCache createTileCache(int capacity) {
-    TileCache firstLevelTileCache = new InMemoryTileCache(capacity);
+  private TileCache createTileCache() {
+    TileCache firstLevelTileCache = new InMemoryTileCache(64);
     File cacheDirectory = new File(System.getProperty("java.io.tmpdir"), "mapsforge");
     TileCache secondLevelTileCache = new FileSystemTileCache(1024, cacheDirectory, GRAPHIC_FACTORY);
     return new TwoLevelTileCache(firstLevelTileCache, secondLevelTileCache);
@@ -225,74 +194,31 @@ public final class Visualizer {
     return tileRendererLayer;
   }
 
-  private class ConvexLayer extends Polyline {
+  private class DotsLayer extends Layer {
 
-    public ConvexLayer(GraphicFactory graphicFactory, int pathColor,
-        float pathStrokeWidth, ArrayList<LatLong> dots) {
-      super(null, graphicFactory);
-      org.mapsforge.core.graphics.Paint paintStroke = GRAPHIC_FACTORY.createPaint();
-      paintStroke.setStyle(Style.STROKE);
-      paintStroke.setColor(pathColor);
-      paintStroke.setStrokeWidth(pathStrokeWidth);
-      this.setPaintStroke(paintStroke);
-      List<LatLong> convex = MapPoint
-          .convertToLatlong(Convex.getConvex(MapPoint.convertFromLatlong(dots)));
-      if (convex != null) {
-        super.getLatLongs().addAll(convex);
-      } else {
-        System.out.println("got null");
-      }
-    }
-  }
+    private ArrayList<LatLong> mDots;
+    private int mRadius;
 
-  private class LineLayer extends Polyline {
-
-    private HashMap<LatLong, Integer> mDots;
-    private List<LatLong> mPath;
-
-    public LineLayer(GraphicFactory graphicFactory, HashMap<LatLong, Integer> dotWithColor,
-        int pathColor,
-        float pathStrokeWidth, List<LatLong> path) {
-      super(null, graphicFactory);
-      org.mapsforge.core.graphics.Paint paintStroke = GRAPHIC_FACTORY.createPaint();
-      paintStroke.setStyle(Style.STROKE);
-      paintStroke.setColor(pathColor);
-      paintStroke.setStrokeWidth(pathStrokeWidth);
-      this.setPaintStroke(paintStroke);
-      mDots = dotWithColor;
-      mPath = path;
-
-      super.getLatLongs().addAll(mPath);
+    DotsLayer(ArrayList<LatLong> dots, int radius) {
+      mDots = dots;
+      mRadius = radius;
     }
 
     @Override
     public synchronized void draw(BoundingBox boundingBox, byte zoomLevel,
         org.mapsforge.core.graphics.Canvas canvas, org.mapsforge.core.model.Point topLeftPoint) {
-      super.draw(boundingBox, zoomLevel, canvas, topLeftPoint);
       long mapSize = MercatorProjection.getMapSize(zoomLevel, displayModel.getTileSize());
-      // Draw the points
-      for (LatLong latLong : mDots.keySet()) {
+      /* Draw the points */
+      for (LatLong latLong : mDots) {
         int pixelX = (int) (MercatorProjection.longitudeToPixelX(latLong.longitude, mapSize)
             - topLeftPoint.x);
         int pixelY = (int) (MercatorProjection.latitudeToPixelY(latLong.latitude, mapSize)
             - topLeftPoint.y);
         org.mapsforge.core.graphics.Paint paintStroke = GRAPHIC_FACTORY.createPaint();
-        paintStroke.setStyle(Style.STROKE);
-        paintStroke.setColor(mDots.get(latLong));
-        paintStroke.setStrokeWidth(6.0f);
-        canvas.drawCircle(pixelX, pixelY, 3, paintStroke);
+        paintStroke.setStyle(Style.FILL);
+        paintStroke.setColor(DOTCOLOR.getRGB());
+        canvas.drawCircle(pixelX, pixelY, mRadius, paintStroke);
       }
-    }
-
-    public double contains(LatLong point) {
-      double threshold = 0.01;
-      for (LatLong dot : mPath) {
-        double dist = point.distance(dot);
-        if (dist < threshold) {
-          return dist;
-        }
-      }
-      return -1.0;
     }
   }
 }
