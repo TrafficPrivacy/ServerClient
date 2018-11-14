3b4256e971a80a5d9b05ce6187169fd3dc589f31
chenqingrong <chenqingrong@sjtu.edu.cn>
2018-11-13 20:15:47 -0600

    changing for considering both poi and partition size

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,13 +1,18 @@
 package server;
 
+import com.graphhopper.routing.util.EdgeFilter;
+import com.graphhopper.storage.NodeAccess;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import util.*;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * The server is responsible to compute three matrices: 1. The all to border shortest path in the
@@ -18,7 +23,8 @@ public class Server {
 
   private ServerSocket mServer;
   private MatrixComputer mMatrixComputer;
-
+  private FileOutputStream moutput_parition;
+  private FileOutputStream moutput_partition_two;
   private final double RADIUS = 1000.0;
   private final double LATRANGE = 0.005;
   private final double LONRANGE = 0.007;
@@ -26,6 +32,9 @@ public class Server {
   public Server(int port, String osmPath, String osmFolder, String strategy) throws IOException {
     mServer = new ServerSocket(port);
     mMatrixComputer = new MatrixComputer(osmPath, osmFolder, strategy);
+    moutput_parition= new FileOutputStream("group_size_for_square_partition.txt");
+    moutput_partition_two= new FileOutputStream("poi_size_for_square_partition.txt");
+
   }
 
   public void run() {
@@ -62,7 +71,9 @@ public class Server {
     out.close();
   }
 
-  private Reply calculate(MapPoint src, MapPoint dst) {
+
+
+  private Reply calculate(MapPoint src, MapPoint dst) throws IOException {
     Pair<int[], int[]> srcRegion;
     Pair<int[], int[]> dstRegion;
     try {
@@ -76,12 +87,25 @@ public class Server {
     }
     int endCenter = mMatrixComputer.getmSurroundings().lookupNearest(dst.getLat(), dst.getLon());
     /*TODO: refactor this*/
+
+
+
+
     Paths srcPaths = mMatrixComputer
         .set2Set(srcRegion.mFirst, srcRegion.mSecond, false, endCenter, RADIUS);
     Paths dstPaths = mMatrixComputer
         .set2Set(dstRegion.mSecond, dstRegion.mFirst, false, endCenter, RADIUS);
     Paths interPaths = mMatrixComputer
         .set2Set(srcRegion.mSecond, dstRegion.mSecond, true, endCenter, RADIUS);
+
+    String tmp=srcRegion.mFirst.length+"\n";
+    moutput_parition.write(tmp.getBytes());
+    tmp=dstRegion.mFirst.length+"\n";
+    moutput_parition.write(tmp.getBytes());
+
+    tmp=mMatrixComputer.POI_count(srcRegion.mFirst,mMatrixComputer.nodeAccess_for_out_usage)+"\n";
+    moutput_partition_two.write(tmp.getBytes());
+
 
     return new Reply(srcRegion, dstRegion, srcPaths, dstPaths, interPaths, Reply.OK);
   }
